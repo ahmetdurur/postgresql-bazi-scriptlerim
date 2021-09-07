a01930d2fb48c709744d5a8270296ec5f1c98a4f
Ahmet Durur <ahmet.durur@outlook.com>
2021-09-07 22:38:02 +0300

    transaction ekle prosedürünün kullanımına açıklama eklendi

--- transaction_ekle_ornek_kullanim.sql
+++ transaction_ekle_ornek_kullanim.sql
@@ -1,11 +1,11 @@
 call transaction_ekle
 (
-  '0x075898a49217989d42b01f64af3569d9eaca004a00a49e4f4ea697b1316a1e1a', 
-  12906889, 
-  '0xea674fdde714fd979de3edf0f56aa9716b898ec8', 
-  '0xd42da6f6790467689ae1c8e4db97cd08c22f6e43', 
-  0.08602,
-  0.00002,
-  1.0, 
-  '2021-07-27 10:43'
+  '0x075898a49217989d42b01f64af3569d9eaca004a00a49e4f4ea697b1316a1e1a',  -- hash
+  12906889, 															 -- block number
+  '0xea674fdde714fd979de3edf0f56aa9716b898ec8', 						 -- gonderici cüzdan adres
+  '0xd42da6f6790467689ae1c8e4db97cd08c22f6e43', 						 -- alici cüzdan adres	
+  0.08602,																 -- eth miktari
+  0.00002,																 -- transaction fee
+  1.0,																	 -- gas price (gwei) 
+  '2021-07-27 10:43'													 -- transaction gerçekleşme tarihi
 );
