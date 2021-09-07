922a841d1d91b7d0966ec3d535fcf6e90654ca77
Ahmet Durur <ahmet.durur@outlook.com>
2021-09-07 23:19:09 +0300

    call_eth_gelir_ekle_ornek_kullanim.sql eklendi

---
+++ call_eth_gelir_ekle_ornek_kullanim.sql
@@ -0,0 +1,8 @@
+call eth_gelir_ekle 
+(
+	'23.12.2022',       -- tarih
+	1.823 , 			-- havuz biriken
+	0.00532 , 			-- gunluk gelir
+	null , 				-- varsa açıklama
+	false				-- payout günüyse true değilse false
+)
