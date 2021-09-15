6bb84bddb835f33fce5e7811de25ceabc4efb9de
Ahmet Durur <ahmet.durur@outlook.com>
2021-09-15 03:56:21 +0300

    iki_transaction_arasi_gunluk_ortalama_gelir_ornek_kullanim.sql eklendi

---
+++ iki_transaction_arasi_gunluk_ortalama_gelir_ornek_kullanim.sql
@@ -0,0 +1,3 @@
+select * from iki_transaction_arasi_gunluk_ortalama_gelir(11,12)
+-- parametre1 ve parametre2 ardışık iki transactionın id'si olmalıdır eğer ardışık değilse ortalama hesabı yanlış olur
+-- parametre1 öncekiid parametre2 sonraki id olmalıdır.  
