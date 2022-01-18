b1544d4ed2eda26cfe5247d4173fbaaa34286751
Ahmet Durur <ahmet.durur@outlook.com>
2022-01-18 17:57:47 +0300

    trigera master durum güncellemesi eklendi

--- tri_detail_to_master.sql
+++ tri_detail_to_master.sql
@@ -5,11 +5,18 @@ as $BODY$
 declare  master_id numeric = new.t_master_id;
 		 acik_pozisyon numeric ;
 		 kapatilan_miktar numeric;
-begin 	
+		 durum numeric; 
+begin 	 
 		 kapatilan_miktar = new.t_satilan_coin_miktar;
-		 acik_pozisyon = (select t_acik_pozisyon_miktar from crypto_trade_master where t_id = master_id);	
-		 update crypto_trade_master set t_acik_pozisyon_miktar = (acik_pozisyon - kapatilan_miktar ) where t_id = master_id ; 
+		 acik_pozisyon = (select t_acik_pozisyon_coin_miktar from crypto_trade_master where t_id = master_id);	
+		 if(acik_pozisyon - kapatilan_miktar = acik_pozisyon)  then durum = 1 ; 
+		 elseif (acik_pozisyon - kapatilan_miktar > 0) then durum = 3;
+		 elseif (acik_pozisyon - kapatilan_miktar = 0 ) then durum = 2;
+		 end if;
+		 	update crypto_trade_master set t_acik_pozisyon_coin_miktar = (acik_pozisyon - kapatilan_miktar ) ,
+		 	t_trade_durum = durum 
+			where t_id = master_id ; 
 		 return null ;
 end
-$BODY$
+$BODY$ ;
 	
